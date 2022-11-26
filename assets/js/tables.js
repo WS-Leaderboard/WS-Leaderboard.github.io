b2a92fd29f5f693351d1c5a52edd0a67a32ec208
exa18 <julian.cenkier@wp.eu>
2022-11-26 05:13:15 +0100

    PHP version to make HTML working table
    Added Table.js sorting
    Added make HTML php functions.

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -4,88 +4,10 @@
 	Convert csv to json:
 	https://www.convertcsv.com/csv-to-json.htm
 */
-var caretUpClassName = 'fa fa-caret-up';
-var caretDownClassName = 'fa fa-caret-down';
+
 var table = document.getElementById('full_list');
 var input = document.getElementById('myinput');
 
-var tableData = rankingsData;
-
-const sort_by = (field, reverse, primer) => {
-
-	const sort_reverse = ["rank", "corporation"];
-	
-	const key = primer ?
-	function(x) {
-	  return primer(x[field]);
-	} :
-	function(x) {
-	  return x[field];
-	};
-
-	if (sort_reverse.includes(field)) {
-		reverse = !reverse ? 1 : -1;
-	}
-	else {
-		reverse = !reverse ? -1 : 1;
-	}
-	
-	return function(a, b) {
-	return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-	};
-};
-
-function clearArrow() {
-	let carets = document.getElementsByClassName('caret');
-	for (let caret of carets) {
-		caret.className = "caret";
-	}
-}
-
-function toggleArrow(event) {
-	let element = event.target;
-	let caret, field, reverse;
-	if (element.tagName === 'SPAN') {
-		caret = element.getElementsByClassName('caret')[0];
-		field = element.id
-	}
-	else {
-		caret = element;
-		field = element.parentElement.id
-	}
-
-	let iconClassName = caret.className;
-	clearArrow();
-	if (iconClassName.includes(caretDownClassName)) {
-		caret.className = `caret ${caretUpClassName}`;
-		reverse = true;
-	} else {
-		reverse = false;
-		caret.className = `caret ${caretDownClassName}`;
-	}
-
-	tableData.sort(sort_by(field, reverse));
-	populateTable();
-}
-function populateTable() {
-	table.innerHTML = '';
-	for (let data of tableData) {
-		let row = table.insertRow(-1);
-		let rank = row.insertCell(0);
-		rank.innerHTML = data.rank;
-
-		let corporation = row.insertCell(1);
-		corporation.innerHTML = data.corporation;
-
-		let rating = row.insertCell(2);
-		rating.innerHTML = data.rating;
-
-		let delta = row.insertCell(3);
-		delta.innerHTML = data.delta;
-	}
-
-	filterTable();
-}
 
 function filterTable() {
 	let filter = input.value.toUpperCase();
@@ -111,16 +33,20 @@ function filterTable() {
 	}
 }
 
-populateTable();
-
-let tableColumns = document.getElementsByClassName('table-column');
-
-for (let column of tableColumns) {
-	column.addEventListener('click', function(event) {
-		toggleArrow(event);
-	});
-}
+filterTable();
 
 input.addEventListener('keyup', function(event) {
 	filterTable();
 });
+
+
+var sortable = $('table.table-wrapper');
+
+$(document).ready(function(){
+	sortable.tablesorter({
+		sortList: [[0,0]],
+		headers: {4:{sorter:false}}
+	});
+});
+
+	
