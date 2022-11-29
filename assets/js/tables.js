9d0d71c8b30b5f0281fabed15a9312f424ddda4d
exa18 <julian.cenkier@wp.eu>
2022-11-29 19:28:48 +0100

    some cleanings

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -1,48 +1,22 @@
 /*
-	Modified code derived from various internet sources, primarily:
-	https://betterprogramming.pub/sort-and-filter-dynamic-data-in-table-with-javascript-e7a1d2025e3c
-	Convert csv to json:
-	https://www.convertcsv.com/csv-to-json.htm
+	Table content async loader
+	*
+	depence: jQuery
 */
-
-function filterTable() {
-	let filter = input.value.toUpperCase();
-	rows = table.getElementsByTagName("TR");
-	let flag = false;
-
-	for (let row of rows) {
-	let cells = row.getElementsByTagName("TD");
-	for (let cell of cells) {
-		if (cell.textContent.toUpperCase().indexOf(filter) > -1) {
-			flag = true;
-			break;
-		}
-	}
-
-	if (flag) {
-		row.style.display = "";
-	} else {
-		row.style.display = "none";
-	}
-
-	flag = false;
-	}
-}
-
-/* filterTable();
-input.addEventListener('keyup', function(event) {
-	filterTable();
-});
- */
-
 var rankings = $('table.table-wrapper.rankings');
 var games = $('table.table-wrapper.games');
 var input = $('#myinput');
+var loading = "LOADING...";
 
 $(document).ready(function(){
-	$('table.table-wrapper').html("LOADING...");
+	/*
+		Paste Loading indicator, nothing fancy instead animated gif
+	*/
+	$('table.table-wrapper').html(loading);
 	/*
 		check IF github.io
+		then load static HTML, which should be synced with PHP
+		or load PHP on hosting which support it
 	*/
 	site = document.location;
 	if ( site.toString().includes("github.io")) {
@@ -51,17 +25,20 @@ $(document).ready(function(){
 		file = 'php';
 	}
 	/*
-		Load content or dynamic PHP or static
-		create static (linux console)
-		curl -L "http://ws-leaderboard.h2g.pl/site/games.php" > ./site/games.html
-		curl -L "http://ws-leaderboard.h2g.pl/site/rankings.php" > ./site/rankings.html
-
+		Load content or dynamic PHP or static, need to be in main directory
+		Linux/terminal
+			curl -L "http://ws-leaderboard.h2g.pl/site/games.php" > ./site/games.html
+			curl -L "http://ws-leaderboard.h2g.pl/site/rankings.php" > ./site/rankings.html
+		or Powershell (terminal)
+			wget "http://ws-leaderboard.h2g.pl/site/games.php"  -outfile "./site/games.html"
+			wget "http://ws-leaderboard.h2g.pl/site/rankings.php" -outfile "./site/rankings.html"
 	*/
 	if (rankings.length > 0){
 		$.get('site/rankings.'+file, function(data) {
 			rankings.html(data);
 			rankings.tablesorter({
-				sortList: [[0,0]],
+				// Start sort by Rating DESC
+				sortList: [[2,1]],
 			});
 		});
 		table = rankings;
@@ -70,6 +47,7 @@ $(document).ready(function(){
 		$.get('site/games.'+file, function(data) {
 			games.html(data);
 			games.tablesorter({
+				// Start sort by Date DESC
 				sortList: [[5,1]],
 				// sortInitialOrder: "desc",
 				//headers: {5 : {sorter:true, sortInitialOrder: "desc"} }
@@ -77,10 +55,13 @@ $(document).ready(function(){
 		});
 		table = games;
 	}
+	/*
+		Add event on search field (instead listener)
+	*/
 	input.on("keyup", function() {
 		var value = $(this).val().toLowerCase();
 		table.find('tbody tr').filter(function() {
-		  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 		});
 	  });
 
