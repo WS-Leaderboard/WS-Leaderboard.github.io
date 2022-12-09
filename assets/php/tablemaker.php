7aef9b201944f105e427fd1afb117a9fc37ba749
exa18 <julian.cenkier@wp.eu>
2022-12-09 22:25:39 +0100

    clean: more elegant and ~10% speed improvment

--- assets/php/tablemaker.php
+++ assets/php/tablemaker.php
@@ -1,7 +1,34 @@
 <?php
-	function makeHtmlTable($data){
+/*
+	OBJ
+*/
+class TableMaker {
+	public $data = [];
+	public $headeroveride = [];
+	public $header = [];
+	public $caret = '<i class="caret fa fa-caret-up"></i><i class="caret fa fa-caret-down"></i>';
+
+	function __construct($f, $overide=[]) {
+		$this->data = $this->csvToArray($f, ',');
+		if ( count($this->data) > 1 && is_array($this->data[0]) ) {
+			$this->header = $this->data[0];
+		}
+		$this->headeroveride = $overide;
+	}
+
+	public function makeHeader(){
 		$html = [];
-		foreach ($data as $k => $tr) {
+		$ov = $this->headeroveride;
+		$html[] = "<tr>";
+		foreach( $this->header as $k => $v ){
+			$html[] = '<th class="tablesorter-header tablesorter-headerUnSorted"><span>'.( isset($ov[$k]) ? $ov[$k] : ucfirst($k) ). $this->caret . '</span></th>';
+		}
+		$html[] = "</tr>";
+		return implode('',$html);
+    }
+	public function makeBody(){
+		$html = [];
+		foreach ($this->data as $k => $tr) {
 			$html[] = "<tr>";
 			foreach($tr as $td){
 				$html[] = "<td>".trim($td)."</td>";
@@ -10,25 +37,10 @@
 		}
 		return implode('',$html);
 	}
-	function makeHtmlCaret(){
-		return '<i class="caret fa fa-caret-up"></i><i class="caret fa fa-caret-down"></i>';
-	}
-    function makeHtmlTableHeader($data,$overide=[]){
-        $html = [];
-		if ( count($data) > 1 && is_array($data[0]) ) {
-			$data = $data[0];
-		}
-        $html[] = "<tr>";
-        foreach( $data as $k => $v ){
-            $html[] = '<th class="tablesorter-header tablesorter-headerUnSorted"><span>'.( isset($overide[$k]) ? $overide[$k] : ucfirst($k) ). makeHtmlCaret() . '</span></th>';
-        }
-        $html[] = "</tr>";
-        return implode('',$html);
-    }
 	/**
 	* @link http://gist.github.com/385876
 	*/
-	function csvToArray($filename='', $delimiter=';')
+	private function csvToArray($filename='', $delimiter=';')
 	{
 		if(!file_exists($filename) || !is_readable($filename))
 			return FALSE;
@@ -49,4 +61,7 @@
 		}
 		return $data;
 	}
+}
+
+
 ?>
--- site/games.php
+++ site/games.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-include '../assets/php/tablemaker.php';
+include_once '../assets/php/tablemaker.php';
 
 $f = '../data/full_archive.csv';
-$games = csvToArray($f, ',');
 $header = ["team1"=>"Team 1", "score1"=>"Score 1", "score2"=>"Score 2", "team2"=>"Team 2"];
-echo '<thead>' . makeHtmlTableHeader($games,$header) . '</thead><tbody id="full_list">' . makeHtmlTable($games) . '</tbody>';
+$t = new TableMaker($f,$header);
+echo '<thead>' . $t->makeHeader() . '</thead><tbody id="full_list">' . $t->makeBody() . '</tbody>';
 ?>
