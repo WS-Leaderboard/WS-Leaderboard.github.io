8d45c10acb40609fa29f10891a99137a25b116b5
exa18 <julian.cenkier@wp.eu>
2023-02-12 21:02:35 +0100

    fix
    Cleaning move  making header to construct.

--- assets/php/TableMaker.php
+++ assets/php/TableMaker.php
@@ -4,24 +4,24 @@
 */
 class TableMaker {
 	public $data = [];
-	private $headeroveride = [];
 	public $header = [];
 	public $caret = '<i class="caret fa fa-caret-up"></i><i class="caret fa fa-caret-down"></i>';
 
-	function __construct($f, $overide=[], $delimiter=',') {
-		$this->csvToArray(WSL_ROOT . $f, $delimiter);
+	function __construct($f, $overide=[]) {
+		$this->csvToArray(WSL_ROOT . $f, WSL_DELIMITER);
 		if ( count($this->data) > 1 && is_array($this->data[0]) ) {
 			$this->header = $this->data[0];
 		}
-		$this->headeroveride = $overide;
+		foreach( $this->header as $k => $v ){
+			$this->header[$k] = ( isset($overide[$k]) ? $overide[$k] : ucfirst($k) );
+		}
 	}
 
 	public function makeHeader(){
 		$html = [];
-		$ov = $this->headeroveride;
 		$html[] = "<tr>";
 		foreach( $this->header as $k => $v ){
-			$html[] = '<th class="tablesorter-header tablesorter-headerUnSorted"><span>'.( isset($ov[$k]) ? $ov[$k] : ucfirst($k) ). $this->caret . '</span></th>';
+			$html[] = '<th class="tablesorter-header tablesorter-headerUnSorted"><span>'.$v. $this->caret . '</span></th>';
 		}
 		$html[] = "</tr>";
 		return implode('',$html);
