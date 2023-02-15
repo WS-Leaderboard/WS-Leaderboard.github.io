fe8ab95395c8885ed96a95998806d1e8ffebbb3f
exa18 <julian.cenkier@wp.eu>
2023-02-15 08:08:58 +0100

    update load fonts
    Moved loading fonts to html head instead of @import.

--- assets/php/TemplateMaker.php
+++ assets/php/TemplateMaker.php
@@ -30,7 +30,10 @@ class TemplateMaker{
 		<head>
 			<title>'.$title.'</title>
 			<meta http-equiv="content-type" content="text/html; charset="utf-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />'.
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;700;900&display=swap" rel="stylesheet">'.
 			self::Link(['main.min.css','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'])
 			.'<noscript>'.self::Link('noscript.min.css').'</noscript>
 			<link rel="icon" type="image/png" href="/images/favicon.png">
